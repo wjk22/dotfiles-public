8b7061871e7f0e02e2e0ecadcbbc84f4e6d47eb8
Wojciech J. Kalka <wjk@sn-s.net>
2022-04-06 01:12:56 +0700

    Comments

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bas
 
+#Install OH MY ZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+#Install plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+#Install theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
