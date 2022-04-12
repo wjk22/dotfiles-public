9df676b73165b609d46c0892fce5519d9ca33f18
Wojciech J. Kalka <wjk@sn-s.net>
2022-04-12 14:18:37 +0700

    Added a basic linux install script

--- linux/install.sh
+++ linux/install.sh
@@ -1,11 +1,42 @@
-#!/bin/bas
+#!/bin/bash
+
+echo "Installing Tek's environment....."
 
 #Install OH MY ZSH
+echo "Installing OH MY ZSH"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sleep 1 
+echo "Installing OH MY ZSH Plugins"
 
 #Install plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+sleep 1
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "Installing P10K"
 
 #Install theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+sleep 1
+
+
+useConfirm=true
+
+confirm() {
+   [ "$useConfirm" = true ] && read -p "Proceed? (Enter) - (^C to abort)"
+}
+
+echo "Copying settings" 
+confirm
+
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+echo "Current Time : $current_time"
+
+mv -v ~/.zshrc ~/zshrc.$current_time
+cp -v .zshrc ~/.zhrc
+cp -v .alias ~/.alias
+cp -v .p10k.zsh ~/.p10k.zsh
