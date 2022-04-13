f9503e370905fe31bb48e64fd1322408867d6ea5
Wojciech J. Kalka <wjk@sn-s.net>
2022-04-13 15:10:57 +0700

    Small fixes

--- linux/install.sh
+++ linux/install.sh
@@ -52,12 +52,12 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 sleep 1
 $red ; echo "Installing MesloLGS Font"
 
-mdkir -v -p ~/.local/share/fonts
+mkdir -v -p ~/.local/share/fonts
 cd ~/.local/share.fonts
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
 ls  -l
 fc-cache-f
 
