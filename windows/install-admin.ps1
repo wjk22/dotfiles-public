06aee9231180f293a87b79dc324e140095f0cd57
Wojciech J. Kalka <wjk@sn-s.net>
2022-04-13 17:59:19 +0700

    Small fixed

--- windows/install-admin.ps1
+++ windows/install-admin.ps1
@@ -6,5 +6,5 @@ Write-Output "Installing Chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 Write-Output "ADMIN install .... installing basic programs with choco"
-choco install git totalcommander vim wox powershell-core everyting gsudo
+choco install git totalcommander vim wox powershell-core everything gsudo
 Install-Module -Name PSWindowsUpdate -Force
--- windows/install.ps1
+++ windows/install.ps1
@@ -9,7 +9,9 @@ scoop install curl fzf
 
 Write-Output "Installing some windows modules .."
 
+Install-Module -Name Terminal-Icons -Repository PSGallery
 Install-Module oh-my-posh -Scope CurrentUser -Force -Verbose
 Install-Module posh-git -Scope CurrentUser -Force -Verbose
 Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentuSer -Force -SkipPublisherCheck -Verbose
 Install-Module -Name PSFzf -Scope CurrentUser -Force -Verbose
+Install-Module -Name z -Scope CurrentUser -Force -Verbose
