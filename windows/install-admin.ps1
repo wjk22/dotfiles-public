f7e0c6de35ce561dd36ba074ecbf387d745ed3f5
Wojciech J. Kalka <wjk@sn-s.net>
2022-04-13 17:42:28 +0700

    Small fixes

--- windows/install-admin.ps1
+++ windows/install-admin.ps1
@@ -2,4 +2,9 @@
 #Basic Admin Windows Install script
 #
 
+Write-Output "Installing Chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Write-Output "ADMIN install .... installing basic programs with choco"
 choco install git totalcommander vim wox powershell-core everyting gsudo
+Install-Module -Name PSWindowsUpdate -Force
--- windows/install.ps1
+++ windows/install.ps1
@@ -2,11 +2,14 @@
 #Basic Windows Use Install script
 #
 #
+Write-Output "Installing scoop"
+
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 scoop install curl fzf
-Install-Module oh-my-posh -Scope CurrentUser -Force
-Install-Module posh-git -Scope CurrentUser -Force
-Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentuSer -Force -SkipPublisherCheck
-Install-Module -Name PSFzf -Scope CurrentUser -Force
-Install-Module -Name PSWindowsUpdate -Force
 
+Write-Output "Installing some windows modules .."
+
+Install-Module oh-my-posh -Scope CurrentUser -Force -Verbose
+Install-Module posh-git -Scope CurrentUser -Force -Verbose
+Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentuSer -Force -SkipPublisherCheck -Verbose
+Install-Module -Name PSFzf -Scope CurrentUser -Force -Verbose
